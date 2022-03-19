f15b5880fc3d228ebce44b756a7ab25b4160efc6
Илья Нечаев <dolphins.mind777@gmail.com>
2022-03-19 17:33:41 +0300

    Initial Commit

---
+++ CoreData App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData App
+//
+//  Created by Илья Нечаев on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
