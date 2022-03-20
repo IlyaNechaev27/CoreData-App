2a5f14896e9b3e41a81f155be547527a5187c5c8
Илья Нечаев <dolphins.mind777@gmail.com>
2022-03-20 18:36:10 +0300

    create "Add Task" button

---
+++ CoreData App/TaskViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TaskViewController.swift
+//  CoreData App
+//
+//  Created by Илья Нечаев on 20.03.2022.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        
+    }
+
+}
