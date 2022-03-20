dc5cdf8235781ff349b3aedddfae227ab0301146
Илья Нечаев <dolphins.mind777@gmail.com>
2022-03-20 18:58:29 +0300

    setup TextField into TaskVC

--- CoreData App/TaskViewController.swift
+++ CoreData App/TaskViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class TaskViewController: UIViewController {
-
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "New Task"
+        textField.borderStyle = .roundedRect
+        
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
-        
+        setupSubviews(textField)
+        setConstraints()
+    }
+    
+    private func setupSubviews(_ subviews: UIView...) {
+        subviews.forEach { subview in
+            view.addSubview(subview)
+        }
+    }
+    
+    private func setConstraints() {
+        textField.translatesAutoresizingMaskIntoConstraints = false
     }
 
 }
