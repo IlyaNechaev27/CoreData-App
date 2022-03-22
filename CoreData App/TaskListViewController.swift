ddd6b8d86aea04ecb29ca92b0bcedbb8ead08962
Ilya Nechaev <dolphins.mind777@gmail.com>
2022-03-23 00:05:57 +0300

    fetchData with CoreData

--- CoreData App/TaskListViewController.swift
+++ CoreData App/TaskListViewController.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import CoreData
 
 class TaskListViewController: UITableViewController {
-
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private let cellID = "task"
+    
+    private var taskList: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         view.backgroundColor = .white
         setupNavigationBar()
+        fetchData()
     }
     
     private func setupNavigationBar() {
@@ -46,5 +53,32 @@ class TaskListViewController: UITableViewController {
         let taskVC = TaskViewController()
         present(taskVC, animated: true)
     }
+    
+    private func fetchData() {
+        let fetchRequest = Task.fetchRequest()
+        
+        do {
+            taskList = try context.fetch(fetchRequest)
+        } catch let error {
+            print(error)
+        }
+    }
 }
 
+// MARK: - UITableViewDataSource
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
